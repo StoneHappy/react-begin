2cc58a4aaf9e827144c4f0f131a60da2098e44a6
xhguleixin123 <18017158729@163.com>
2022-05-26 20:37:14 +0800

    ✨ feat: add css loader

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -3,30 +3,38 @@ const path = require('path');
 
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, '../src/App.js'),
-  },
-  output:{
-    filename:'bundle.js',
-    path:path.resolve(__dirname, '../dist')
-  },
+    entry: {
+        main: path.resolve(__dirname, '../src/App.js'),
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../src/tample.html')
-    })
-  ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/tample.html')
+        })
+    ],
 
-  module: {
-    rules: [
-      {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use:
-              [
-                  'babel-loader'
-              ]
-      },
-    ]
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                    [
+                        'babel-loader'
+                    ]
+            },
+            {
+                test: /\.css$/,
+                use:
+                    [
+                        'style-loader',
+                        'css-loader'
+                    ]
+            },
+        ]
+    },
 };
