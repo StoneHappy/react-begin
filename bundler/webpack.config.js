cda65d3b5ee4d86f40b2eac16ed2504959b879fb
xhguleixin123 <18017158729@163.com>
2022-05-26 20:25:05 +0800

    ✨ feat: add div config

---
+++ bundler/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../src/App.js'),
+  },
+  output:{
+    filename:'bundle.js',
+    path:path.resolve(__dirname, '../dist')
+  },
+  mode: 'development',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/tample.html')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use:
+              [
+                  'babel-loader'
+              ]
+      },
+    ]
+  },
+};
